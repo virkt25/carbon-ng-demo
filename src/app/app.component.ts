b8c0287257bd448b506bf9cdeaabff91345faca7
Taranveer Virk <taranveer@ca.ibm.com>
2020-06-05 17:02:18 -0400

    feat(ui): demo issues for dropdown and combobox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ComboBox } from 'carbon-components-angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'demo';
+  @ViewChild('geoCombo') geoCombo: ComboBox;
+
+  flatPickrOptions = {
+    minDate: '2020-06-10',
+    maxDate: '2020-06-20'
+  }
+
+  geos: Geo[] = [{content: 'Toronto', selected: false, data: 1}, {content: 'Montreal', selected: false, data: 1}, {content: 'Vancouver', selected: false, data: 1}, {content: 'Ottawa', selected: false, data: 1}]
+
+  selectGeo(geo) {
+    console.log({geo})
+  }
+
+  clearGeo() {
+    this.geos = this.geos.map(geo => ({ ...geo, selected: false }));
+    console.log({geos: this.geos})
+  }
+
+  clearGeoComplex() {
+    this.geos = this.geos.map((geo) => ({ ...geo, selected: false }));
+
+    setTimeout(() => {
+      this.geoCombo.showClearButton = false;
+      this.geoCombo.selectedValue = '';
+      this.geoCombo.clearSelected();
+    }, 0);
+  }
+}
+
+export interface Geo {
+  content: string;
+  selected: boolean;
+  data: number;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DatePickerModule, ComboBoxModule } from 'carbon-components-angular';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    DatePickerModule,
+    ComboBoxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
